a6050e9c42969c68386af9ad077d4204200b32f6
Tatiana <tatianabb21@gmail.com>
2020-09-06 19:48:16 -0500

    Se añadio, cambio de nombre a tema

--- js/theme.js
+++ js/theme.js
@@ -1,8 +1,16 @@
 const check= document.getElementById('checkbox');
+let themeLight = document.querySelector(".light");
+let themeDark = document.querySelector(".dark");
 check.addEventListener('change',e =>{
     if(e.target.checked){
         document.documentElement.setAttribute('theme','dark-theme');
+        themeDark.classList.remove("statusOFF")
+        themeDark.classList.add("statusON");
+        themeLight.classList.add("statusOFF")
     }else{
         document.documentElement.setAttribute('theme','light-theme');
+        themeLight.classList.remove("statusOFF")
+        themeLight.classList.add("statusON");
+        themeDark.classList.add("statusOFF")
     }
 })
