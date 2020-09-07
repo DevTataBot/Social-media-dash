4eade290dfeb736fd584388fd8783119d9c903e8
Tatiana <tatianabb21@gmail.com>
2020-09-06 19:54:56 -0500

    Se agrego h2 oculto

--- js/theme.js
+++ js/theme.js
@@ -3,12 +3,12 @@ let themeLight = document.querySelector(".light");
 let themeDark = document.querySelector(".dark");
 check.addEventListener('change',e =>{
     if(e.target.checked){
-        document.documentElement.setAttribute('theme','dark-theme');
+        document.documentElement.setAttribute('theme','data-dark-theme');
         themeDark.classList.remove("statusOFF")
         themeDark.classList.add("statusON");
         themeLight.classList.add("statusOFF")
     }else{
-        document.documentElement.setAttribute('theme','light-theme');
+        document.documentElement.setAttribute('theme','data-light-theme');
         themeLight.classList.remove("statusOFF")
         themeLight.classList.add("statusON");
         themeDark.classList.add("statusOFF")
